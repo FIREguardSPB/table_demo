bda88f4c3928e1b21f59e45a89ae2effe7a9a026
Anton <stells@ngs.ru>
2021-03-14 10:10:27 +0300

    fix1

--- src/actions/dataAddAction.js
+++ src/actions/dataAddAction.js
@@ -2,10 +2,10 @@ import {addData} from "../redux/reducer";
 import _ from "lodash";
 
 export const getUsers = () => (dispatch, getState) => {
-setTimeout(() => {
+
     fetch('http://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
         .then(response => response.json())
         .then(data => dispatch(addData(_.orderBy(data, 'id', 'asc'))))
-}, 2000)
+
 
 };
