f1ae78cfcecf14ca9c1d99d4ca6087e9d375ac31
Anton <stells@ngs.ru>
2021-03-14 16:02:32 +0300

    fix5

--- src/actions/dataAddAction.js
+++ src/actions/dataAddAction.js
@@ -3,7 +3,13 @@ import _ from "lodash";
 
 export const getUsers = () => (dispatch, getState) => {
 
-    fetch('http://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
+    fetch('http://www.filltext.com/?rows=250&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',{
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        // body: JSON.stringify(payload)
+    })
         .then(response => response.json())
         .then(data => dispatch(addData(_.orderBy(data, 'id', 'asc'))))
 
